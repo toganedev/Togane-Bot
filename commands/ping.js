771981abb622cdec2315b780baed4f4c18d8a137
𝓣𝓸𝓰𝓪𝓷𝓮 <botcreator13@gmail.com>
2025-08-03 18:15:28 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,17 +3,35 @@ import { SlashCommandBuilder, EmbedBuilder } from 'discord.js'
 export default {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('📶 Botの応答Pingを表示します'),
+    .setDescription('📶 Ping情報を表示します'),
 
   async execute(interaction) {
     const sent = await interaction.reply({ content: '📡 Pinging...', fetchReply: true })
 
-    const latency = sent.createdTimestamp - interaction.createdTimestamp
+    const botLatency = sent.createdTimestamp - interaction.createdTimestamp
+    const wsPing = interaction.client.ws.ping
 
     const embed = new EmbedBuilder()
       .setColor('Random')
       .setTitle('🏓 Pong!')
-      .setDescription(`🤖 Bot Latency: **${latency}ms**`)
+      .setDescription('```Ping結果を以下に表示します```')
+      .addFields(
+        {
+          name: '🤖 Bot Latency',
+          value: `${botLatency}ms`,
+          inline: true
+        },
+        {
+          name: '🌐 API Latency',
+          value: wsPing < 0 ? '未接続' : `${wsPing}ms`,
+          inline: true
+        },
+        {
+          name: '🧠 WebSocket Ping',
+          value: wsPing < 0 ? '未接続' : `${wsPing}ms`,
+          inline: true
+        }
+      )
       .setFooter({ text: 'Togane Bot', iconURL: interaction.client.user.displayAvatarURL() })
       .setTimestamp()
 
