82dc2f4870baa4e097a623d6d56dfa1bbeb62415
𝓣𝓸𝓰𝓪𝓷𝓮 <botcreator13@gmail.com>
2025-08-03 19:44:11 +0900

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -19,28 +19,29 @@ export default {
     if (!channelId) return
 
     const channel = member.guild.channels.cache.get(channelId)
-    if (!channel || !channel.isTextBased()) return
+    if (!channel?.isTextBased()) return
 
     const canvas = Canvas.createCanvas(700, 250)
     const ctx = canvas.getContext('2d')
 
-    const background = await Canvas.loadImage('https://i.imgur.com/AfFp7pu.png')
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
-
-    ctx.font = '28px sans-serif'
     ctx.fillStyle = '#ffffff'
-    ctx.fillText(`Welcome, ${member.user.tag}!`, canvas.width / 2.5, canvas.height / 3)
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    ctx.font = '20px sans-serif'
-    ctx.fillText(`User ID: ${member.id}`, canvas.width / 2.5, canvas.height / 2.2)
-    ctx.fillText(`Guild: ${member.guild.name}`, canvas.width / 2.5, canvas.height / 1.8)
+    ctx.font = '32px "DejaVu Sans"'
+    ctx.fillStyle = '#323232'
+    ctx.fillText(`Welcome, ${member.user.tag}!`, 260, 70)
+
+    ctx.font = '20px "DejaVu Sans"'
+    ctx.fillStyle = '#666666'
+    ctx.fillText(`User ID: ${member.id}`, 260, 120)
+    ctx.fillText(`Guild: ${member.guild.name}`, 260, 150)
 
     ctx.beginPath()
     ctx.arc(125, 125, 100, 0, Math.PI * 2, true)
     ctx.closePath()
     ctx.clip()
 
-    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ extension: 'jpg' }))
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ extension: 'jpg', size: 256 }))
     ctx.drawImage(avatar, 25, 25, 200, 200)
 
     const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), {
